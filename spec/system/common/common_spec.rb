891e814c0e29dff62ee9143ade917a64099a1596
DaichiSaito <hrkedz@gmail.com>
2019-07-30 16:09:14 +0900

    [Add]Bootstrapの導入

--- spec/system/common/common_spec.rb
+++ spec/system/common/common_spec.rb
@@ -7,9 +7,15 @@ RSpec.describe '共通系', type: :system do
   describe 'ヘッダー' do
     it 'ヘッダーが正しく表示されていること' do
       expect(page).to have_content '掲示板'
+      expect(page).to have_content 'ブックマーク一覧'
+
+      click_on('掲示板')
       expect(page).to have_content '掲示板一覧'
       expect(page).to have_content '掲示板作成'
-      expect(page).to have_content 'ブックマーク一覧'
+
+      find('#header-profile').click
+      # login_user = User.first
+      # expect(page).to have_content "#{login_user.last_name} #{login_user.first_name}"
       expect(page).to have_content 'プロフィール'
       expect(page).to have_content 'ログアウト'
     end
