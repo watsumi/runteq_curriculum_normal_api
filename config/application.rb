03f69b00269f2a3774f05fb315265556ffb574a9
DaichiSaito <hrkedz@gmail.com>
2019-07-30 12:38:12 +0900

    [Add]アプリの基本設定

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,15 @@ module RunteqNormal
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
