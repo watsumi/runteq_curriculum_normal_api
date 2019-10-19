ff1e8097108f08bfa591ed10cce895c0b46593e1
DaichiSaito <hrkedz@gmail.com>
2019-10-19 13:18:38 +0900

    ユーザー登録画面のスペックを国際化対応用に変更

--- spec/system/users/user_spec.rb
+++ spec/system/users/user_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'ユーザー登録', type: :system do
   context '入力情報正常系' do
     it 'ユーザーが新規作成できること' do
       visit new_user_path
-      fill_in 'Last name', with: 'らんてっく'
-      fill_in 'First name', with: 'たろう'
-      fill_in 'Email', with: 'example@example.com'
-      fill_in 'Password', with: '12345678'
-      fill_in 'Password confirmation', with: '12345678'
+      fill_in '姓', with: 'らんてっく'
+      fill_in '名', with: 'たろう'
+      fill_in 'メールアドレス', with: 'example@example.com'
+      fill_in 'パスワード', with: '12345678'
+      fill_in 'パスワード確認', with: '12345678'
       click_button '登録'
       expect(current_path).to eq login_path
       # expect(current_path).to eq boards_path
@@ -20,7 +20,7 @@ RSpec.describe 'ユーザー登録', type: :system do
   context '入力情報異常系' do
     it 'ユーザーが新規作成できない' do
       visit new_user_path
-      fill_in 'Email', with: 'example@example.com'
+      fill_in 'メールアドレス', with: 'example@example.com'
       click_button '登録'
       expect(current_path).to eq '/users'
       # expect(current_path).to eq boards_path
