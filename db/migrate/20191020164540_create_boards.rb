55ab98a13d3bd38578c89fb3b647b1feeb8bedde
DaichiSaito <hrkedz@gmail.com>
2019-10-21 01:46:54 +0900

    [Add]be rails db:migrate

--- db/migrate/20191020164540_create_boards.rb
+++ db/migrate/20191020164540_create_boards.rb
@@ -1,8 +1,8 @@
 class CreateBoards < ActiveRecord::Migration[5.2]
   def change
     create_table :boards do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_071729) do
+ActiveRecord::Schema.define(version: 2019_10_20_164540) do
+
+  create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_07_30_071729) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "boards", "users"
 end
