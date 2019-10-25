7d24960e32692eece84b0e5125017ae12bd91f0c
DaichiSaito <hrkedz@gmail.com>
2019-10-25 18:56:41 +0900

    [Add]be rails g model comment body:text user:references board:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_131053) do
+ActiveRecord::Schema.define(version: 2019_10_23_065817) do
 
   create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2019_10_21_131053) do
     t.datetime "updated_at", null: false
     t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.bigint "board_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -34,4 +44,6 @@ ActiveRecord::Schema.define(version: 2019_10_21_131053) do
   end
 
   add_foreign_key "boards", "users"
+  add_foreign_key "comments", "boards"
+  add_foreign_key "comments", "users"
 end
