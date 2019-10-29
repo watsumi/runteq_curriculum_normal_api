7743143934b1ada659ee7a4ed02deda34cd2f7df
DaichiSaito <hrkedz@gmail.com>
2019-10-29 13:04:13 +0900

    [Add]コメントのAjax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,30 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
-    if @comment.save
-      redirect_to board_path(@comment.board), success: t('defaults.message.created', item: Comment.model_name.human)
+    @comment.save
+  end
+
+  def update
+    @comment = current_user.comments.find(params[:id])
+    if @comment.update(comment_update_params)
+      render json: { comment: @comment }, status: :ok
     else
-      redirect_to board_path(@comment.board), danger: t('defaults.message.not_created', item: Comment.model_name.human)
+      render json: { comment: @comment, errors: { messages: @comment.errors.full_messages } }, status: :bad_request
     end
   end
 
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
+  end
+
+  private
+
   def comment_params
     params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+  end
+
+  def comment_update_params
+    params.require(:comment).permit(:body)
   end
 end
