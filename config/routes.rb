11751ea29fafce2db77ee26ed24417a2e2dd8b5b
DaichiSaito <hrkedz@gmail.com>
2019-10-30 20:47:05 +0900

    [Add]パスワードリセット機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   end
   resources :bookmarks, only: %i[create destroy]
   resource :profile, only: %i[show edit update]
+  resources :password_resets, only: %i[new create edit update]
 end
