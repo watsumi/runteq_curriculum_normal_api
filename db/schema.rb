cd1c8b7226ce0e231bddb5ff690b3f946b68fd84
DaichiSaito <hrkedz@gmail.com>
2019-10-31 10:53:04 +0900

    [Add]roleカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_095042) do
+ActiveRecord::Schema.define(version: 2019_10_30_160605) do
 
   create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_095042) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
