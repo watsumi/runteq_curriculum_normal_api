7acf388d0a7a665c058e9c873c6b25d8602c7876
DaichiSaito <hrkedz@gmail.com>
2019-11-01 19:28:51 +0900

    [Add]エラーハンドリング実装

--- Gemfile
+++ Gemfile
@@ -52,6 +52,10 @@ gem 'config'
 
 gem 'enum_help'
 
+gem 'exception_notification'
+
+gem 'slack-notifier'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  rescue_from StandardError, with: :render_500
+
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
 
@@ -7,5 +9,11 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     flash[:warning] = t('defaults.message.require_login')
     redirect_to login_path
+  end
+
+  def render_500(e)
+    logger.error(e.message)
+    logger.error(e.backtrace.join("\n"))
+    render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
   end
 end
