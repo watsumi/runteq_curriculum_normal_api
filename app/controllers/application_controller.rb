afd1b50f85a4937e17aebbfc4966b9274fc04123
DaichiSaito <hrkedz@gmail.com>
2019-11-05 15:53:16 +0900

    [Add]ActiveRecord::NotFoundをうまく捕捉できていなかった

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   rescue_from StandardError, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     flash[:warning] = t('defaults.message.require_login')
     redirect_to login_path
+  end
+
+  def render_404
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
   end
 
   def render_500(e)
