2ccdfea5e6254a205249a8533bb844bcb65cd868
DaichiSaito <hrkedz@gmail.com>
2020-02-05 17:32:59 +0900

    [Fix]スペックのメッセージをわかりやすくした

--- spec/system/common/common_spec.rb
+++ spec/system/common/common_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe '共通系', type: :system do
   end
   describe 'ヘッダー' do
     it 'ヘッダーが正しく表示されていること' do
-      expect(page).to have_content '掲示板'
-      expect(page).to have_content '掲示板一覧'
-      expect(page).to have_content '掲示板作成'
-      expect(page).to have_content 'ブックマーク一覧'
-      expect(page).to have_content 'プロフィール'
-      expect(page).to have_content 'ログアウト'
+      expect(page).to have_content('掲示板'), 'ヘッダーに「掲示板」というテキストが表示されていません'
+      expect(page).to have_content('掲示板一覧'), 'ヘッダーに「掲示板一覧」というテキストが表示されていません'
+      expect(page).to have_content('掲示板作成'), 'ヘッダーに「掲示板作成」というテキストが表示されていません'
+      expect(page).to have_content('ブックマーク一覧'), 'ヘッダーに「ブックマーク一覧」というテキストが表示されていません'
+      expect(page).to have_content('プロフィール'), 'ヘッダーに「プロフィール」というテキストが表示されていません'
+      expect(page).to have_content('ログアウト'), 'ヘッダーに「ログアウト」というテキストが表示されていません'
     end
   end
 
   describe 'フッター' do
     it 'フッターが正しく表示されていること' do
-      expect(page).to have_content 'Copyright © 2019. RUNTEQ'
+      expect(page).to have_content('Copyright'), '「Copyright」というテキストが表示されていません'
     end
   end
 end
